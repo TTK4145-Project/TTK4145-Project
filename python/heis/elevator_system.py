bb3cb8d2d08bb73ab4ad00d7860c4e283149461d
Tri M. Nguyen <mail@trimn.net>
2013-04-26 11:45:15 +0200

    Fix thing.

--- python/heis/elevator_system.py
+++ python/heis/elevator_system.py
@@ -112,9 +112,6 @@ class System:
 
             # filter out the outer orders and delegate them to the other elevators
 
-    def recover(self):
-        pass
-
     def client_reconnected(self, src):
         if src in self.inactive_elevators:
             self.elevators[src] = self.inactive_elevators[src]
